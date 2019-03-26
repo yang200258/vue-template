bf986334005ab6078ffa0971709357f3307ff741
yqq <502258389@qq.com>
2019-03-26 09:26:49 +0800

    2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <article class="article">
+      rem
+    </article>
   </div>
 </template>
 
@@ -11,3 +14,15 @@ export default {
   name: 'home',
 }
 </script>
+
+
+<style lang="scss" scoped>
+  .article {
+    width: 200px;
+    height: 200px;
+    background-color: #999;
+    // text-align: center;
+    line-height: 200px;
+  }
+</style>
+
